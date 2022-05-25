8247626644c29daad0a541cd49ea234c590d1141
evannnns <alt1@trickster.pw>
2022-05-25 11:43:09 +0800

    pwa update

--- pwa.js
+++ pwa.js
@@ -1,7 +1,49 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then((registration) => { 
+         // The registration was successful       
+      })
+      .catch((err) => {
+         // The registration failed
+      });
+  } 
+
+  var urlsToCache = [
+    '/',
+    '/css/app.ea157e18.css',
+    '/js/app.d629961d.js'
+  ];
+  
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache')
+        .then((cache) => {
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
+    );
+  });
+
+
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          // The responce is in the cache
+          if (response) {
+            return response;
+          }
+  
+          // No cache match, we attempt to fetch it from the network
+          return fetch(event.request);
+        }
+      )
     );
   });
